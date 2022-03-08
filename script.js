8b5798a0b3a4136ac6207f91e82a4670b78b4f1c
Abdullahi Ibrahim Saadu <engrabdullahiibrahimsaadu@gmail.com>
2022-03-08 12:31:13 +0100

    Add files via upload
    I added all OPSs File.

---
+++ script.js
@@ -0,0 +1,30 @@
+var button=document.getElementById("enter");
+ var input=document.getElementById("userinput");
+ var ul=document.querySelector("ol");
+ 
+ function inputLength(){
+     return input.value.length;
+ }
+ document.write('Name ');
+ function createListElement(){
+        var li=document.createElement("ol");
+       
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value='';
+    
+ }
+
+ button.addEventListener("click", function(){
+     if( inputLength() >0 ){
+        createListElement();
+     }
+ })
+
+ 
+ 
+ input.addEventListener("keypress", function (event){
+    if( inputLength() >0  && event.keyCode===13){
+       createListElement();
+    }
+})
